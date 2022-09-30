7667cd355c2d772504f1ebc0ecb869df3873cad5
robinsonmourao <49078615+robinsonmourao@users.noreply.github.com>
2022-09-30 14:22:29 -0300

    Initial commit

---
+++ src/test/java/com/a4s/realmeet/RealmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a4s.realmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
